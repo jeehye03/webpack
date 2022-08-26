996dabb9037d44e725287875835abf18413280bd
jeehye03 <ks6247@naver.com>
2022-08-26 10:31:50 +0900

    webpack setting

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function App() {
+  return <div>안녕!</div>;
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.tsx", // 파일의 시작점
+  },
+  output: {
+    // 번들된 파일의 결과물
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  resolve: { extensions: ["*", ".js", ".jsx", ".tsx", ".ts"] }, // import 될 수 있는 파일의 확장자명
+  stats: { children: true },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: true,
+              reloadAll: true,
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "webpack-react-start-kit",
+      template: "./public/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
